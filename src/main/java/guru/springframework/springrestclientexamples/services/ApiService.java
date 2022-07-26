ceaee4769391548e4cbbe7ca551b155fb4634f1e
Cornel Zgardan <cornel.zgardan@modex.tech>
2022-07-26 13:12:57 +0300

    webclient

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiService.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiService.java
@@ -2,7 +2,11 @@ package guru.springframework.springrestclientexamples.services;
 
 import guru.springframework.api.domain.User;
 import java.util.List;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface ApiService {
   List<User> getUsers(Integer limit);
+
+  Flux<User> getUsers(Mono<Integer> limit);
 }
--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -4,26 +4,42 @@ import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+/** Created by jt on 9/21/17. */
 @Service
 public class ApiServiceImpl implements ApiService {
 
   private final RestTemplate restTemplate;
-  private final String apiUri;
+  private final String api_url;
 
-  public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String apiUri) {
+  public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
     this.restTemplate = restTemplate;
-    this.apiUri = apiUri;
+    this.api_url = api_url;
   }
 
   @Override
   public List<User> getUsers(Integer limit) {
-    UriComponentsBuilder uiBuiler =
-        UriComponentsBuilder.fromUriString(apiUri).queryParam("limit", limit);
-    UserData userData = restTemplate.getForObject(uiBuiler.toUriString(), UserData.class);
+
+    UserData userData =
+        restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class);
     return userData.getData();
+  }
+
+  @Override
+  public Flux<User> getUsers(Mono<Integer> limit) {
+
+    return WebClient.create(api_url)
+        .get()
+        .uri(uriBuilder -> uriBuilder.queryParam("limit", limit.block()).build())
+        .accept(MediaType.APPLICATION_JSON)
+        .exchange()
+        .flatMap(resp -> resp.bodyToMono(UserData.class))
+        .flatMapIterable(UserData::getData);
   }
 }
